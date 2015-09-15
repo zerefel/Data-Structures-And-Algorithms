1b2d237051ea3378bd65977f2b3bd6bce4195c97
ZerefeL <zerefel@gmail.com>
2015-09-16 01:59:48 +0300

    Added data structures exam solution to problem 1

---
+++ Data Structures Exam/C#/First-Last-List/FirstLastList.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class FirstLastList<T> : IFirstLastList<T>
+    where T : IComparable<T>
+{
+
+    private List<T> elements = new List<T>();
+
+    public void Add(T newElement)
+    {
+        elements.Add(newElement);
+    }
+
+    public int Count
+    {
+        get { return this.elements.Count; }
+    }
+
+    public IEnumerable<T> First(int count)
+    {
+        if (count > this.elements.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.elements.Take(count);
+    }
+
+    public IEnumerable<T> Last(int count)
+    {
+        if (count > this.elements.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.elements.Skip(Math.Max(0, this.elements.Count() - count)).Reverse();
+    }
+
+    public IEnumerable<T> Min(int count)
+    {
+        if (count > this.elements.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.elements.OrderBy(e => e).Take(count);
+    }
+
+    public IEnumerable<T> Max(int count)
+    {
+        if (count > this.elements.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        var productsFound = this.elements.OrderBy(e => e).Skip(this.elements.Count - count).Reverse();
+
+        return productsFound;
+    }
+
+    public int RemoveAll(T element)
+    {
+        return this.elements.RemoveAll(e => element.CompareTo(e) == 0);
+    }
+
+    public void Clear()
+    {
+        this.elements.Clear();
+    }
+}
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using Wintellect.PowerCollections;
+
+//public class FirstLastList<T> : IFirstLastList<T>
+//    where T : IComparable<T>
+//{
+//    private OrderedBag<T> elements = new OrderedBag<T>();
+//    private LinkedList<T> elementsInOrder = new LinkedList<T>();
+
+//    public void Add(T newElement)
+//    {
+//        elements.Add(newElement);
+//        elementsInOrder.AddLast(newElement);
+//    }
+
+//    public int Count
+//    {
+//        get { return this.elements.Count; }
+//    }
+
+//    public IEnumerable<T> First(int count)
+//    {
+//        if (count > this.elements.Count)
+//        {
+//            throw new ArgumentOutOfRangeException();
+//        }
+
+//        return this.elementsInOrder.Take(count);
+//    }
+
+//    public IEnumerable<T> Last(int count)
+//    {
+//        if (count > this.elements.Count)
+//        {
+//            throw new ArgumentOutOfRangeException();
+//        }
+
+//        return this.elements.Take(count);
+//    }
+
+//    public IEnumerable<T> Min(int count)
+//    {
+//        if (count > this.elements.Count)
+//        {
+//            throw new ArgumentOutOfRangeException();
+//        }
+
+//        return this.elements.Take(count);
+//    }
+
+//    public IEnumerable<T> Max(int count)
+//    {
+//        if (count > this.elements.Count)
+//        {
+//            throw new ArgumentOutOfRangeException();
+//        }
+
+//        var lastElement = this.elements.GetLast();
+
+//        return this.elements.RangeFrom(lastElement, true);
+//    }
+
+//    public int RemoveAll(T element)
+//    {
+//        this.elementsInOrder.RemoveAll(e => e.Equals(element));
+//        return this.elements.RemoveAllCopies(element);
+//    }
+
+//    public void Clear()
+//    {
+//        this.elements.Clear();
+//        this.elementsInOrder.Clear();
+//    }
+//}
---
+++ Data Structures Exam/C#/First-Last-List/LinkedListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class LinkedListExtensions
+{
+    public static void RemoveAll<T>(this LinkedList<T> linkedList,
+                                    Func<T, bool> predicate)
+    {
+        for (LinkedListNode<T> node = linkedList.First; node != null; )
+        {
+            LinkedListNode<T> next = node.Next;
+            if (predicate(node.Value))
+                linkedList.Remove(node);
+            node = next;
+        }
+    }
+}
